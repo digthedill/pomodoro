8d5233ad77b1f654598be2370611e709ba4aab8d
digthedill <popssly@gmail.com>
2020-09-10 18:59:10 -0400

    Updates

---
+++ precache-manifest.e6b623c0376eec2c7c011547eb468b55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752352a2deedade68f8f8586497413fb",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "daadeacc56c69ce2dcfb",
+    "url": "/pomodoro/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "0de1a01e10d15a1369d0",
+    "url": "/pomodoro/static/css/main.8a31a6af.chunk.css"
+  },
+  {
+    "revision": "daadeacc56c69ce2dcfb",
+    "url": "/pomodoro/static/js/2.3416ebbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.3416ebbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de1a01e10d15a1369d0",
+    "url": "/pomodoro/static/js/main.f89c1073.chunk.js"
+  },
+  {
+    "revision": "ee3590aa5d45d2dacf12",
+    "url": "/pomodoro/static/js/runtime-main.f3456f13.js"
+  }
+]);
